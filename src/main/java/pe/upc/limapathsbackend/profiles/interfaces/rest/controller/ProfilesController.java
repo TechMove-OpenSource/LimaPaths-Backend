0795d14770028de5ed09fee61ba1fca6c1d71714
OmBRz <omarbrrcl@gmail.com>
2024-11-04 01:29:44 -0500

    feat(profile): completed interface layer.

---
+++ src/main/java/pe/upc/limapathsbackend/profiles/interfaces/rest/controller/ProfilesController.java
@@ -0,0 +1,55 @@
+package pe.upc.limapathsbackend.profiles.interfaces.rest.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.upc.limapathsbackend.profiles.domain.model.queries.GetProfileByIdQuery;
+import pe.upc.limapathsbackend.profiles.domain.services.ProfileCommandService;
+import pe.upc.limapathsbackend.profiles.domain.services.ProfileQueryService;
+import pe.upc.limapathsbackend.profiles.interfaces.rest.resources.CreateProfileResource;
+import pe.upc.limapathsbackend.profiles.interfaces.rest.resources.ProfileResource;
+import pe.upc.limapathsbackend.profiles.interfaces.rest.transform.CreateProfileCommandFromResourceAssembler;
+import pe.upc.limapathsbackend.profiles.interfaces.rest.transform.ProfileResourceFromEntityAssembler;
+
+/**
+ * ProfilesController
+ * <p>
+ *     This class is the entry point for all the REST endpoints related to the Profile entity.
+ * </p>
+ */
+
+@RestController
+@RequestMapping(value = "/api/v1/profiles")
+@Tag(name="Profiles", description = "Profile Management Endpoints")
+public class ProfilesController {
+
+    private final ProfileQueryService profileQueryService;
+    private final ProfileCommandService profileCommandService;
+
+    public ProfilesController(ProfileQueryService profileQueryService, ProfileCommandService profileCommandService) {
+        this.profileQueryService = profileQueryService;
+        this.profileCommandService = profileCommandService;
+    }
+
+    @GetMapping("/{profileId}")
+    public ResponseEntity<ProfileResource> getProfileById(@PathVariable Long profileId) {
+        var getProfileByIdQuery = new GetProfileByIdQuery(profileId);
+        var profile = profileQueryService.handle(getProfileByIdQuery);
+        if (profile.isEmpty()) return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok(ProfileResourceFromEntityAssembler.transformResourceFromEntity(profile.get()));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProfileResource> createProfile(@RequestBody CreateProfileResource resource) {
+        var createProfileCommand = CreateProfileCommandFromResourceAssembler.toCommandFromResource(resource);
+        var profile = profileCommandService.handle(createProfileCommand);
+
+        if (profile.isEmpty()) return ResponseEntity.badRequest().build();
+
+        var profileResource = ProfileResourceFromEntityAssembler.transformResourceFromEntity(profile.get());
+
+        return new ResponseEntity<ProfileResource>(profileResource, HttpStatus.CREATED);
+    }
+}

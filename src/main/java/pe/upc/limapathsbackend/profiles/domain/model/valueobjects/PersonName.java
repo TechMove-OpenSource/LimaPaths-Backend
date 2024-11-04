a46fc713207343f209f9b6028cf6e46780d6cfa3
OmBRz <omarbrrcl@gmail.com>
2024-11-04 01:21:53 -0500

    feat(profile): Added person name for profile.

---
+++ src/main/java/pe/upc/limapathsbackend/profiles/domain/model/valueobjects/PersonName.java
@@ -0,0 +1,21 @@
+package pe.upc.limapathsbackend.profiles.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+
+@Embeddable
+public record PersonName(@NotBlank String firstName, @NotBlank String lastName) {
+
+    public PersonName {
+        if (firstName == null || firstName.isBlank()) {
+            throw new IllegalArgumentException("firstName cannot be null");
+        }
+        if (lastName == null || lastName.isBlank()) {
+            throw new IllegalArgumentException("lastName cannot be null");
+        }
+    }
+
+    public String getFullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
+}

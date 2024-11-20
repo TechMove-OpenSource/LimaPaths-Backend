190797b5d6403abcee6cdb2e8665f624884ef4fd
Carlos Casemirogit config user.email <u202115412@upc.edu.pe>
2024-11-19 22:57:04 -0500

    properties and config update

--- src/main/java/pe/upc/limapathsbackend/LimaPathsBackendApplication.java
+++ src/main/java/pe/upc/limapathsbackend/LimaPathsBackendApplication.java
@@ -2,6 +2,10 @@ package pe.upc.limapathsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LimaPathsBackendApplication {
@@ -9,5 +13,19 @@ public class LimaPathsBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LimaPathsBackendApplication.class, args);
 	}
-
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
+
+
